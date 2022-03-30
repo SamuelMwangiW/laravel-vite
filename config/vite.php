becd6325403e10cbabcc5aac3029d7231e57ad88
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-03-31 00:09:51 +0300

    Check if Running Laravel Sail to set vite.vite_host (#5)

--- config/vite.php
+++ config/vite.php
@@ -4,8 +4,11 @@ return [
     /**
      * The Vite URL accessible from the web server to vite to fetch modules from
      * This is necessary in local environment to determine the script to load
-     * In docker, this would be "http://host.docker.internal:3000"
+     * In docker, it would be "http://host.docker.internal:3000"
      */
 
-    'vite_host' => env('VITE_URL','http://localhost:3000'),
+    'vite_host' => env(
+        key: 'VITE_URL',
+        default: env(key: 'LARAVEL_SAIL',default: false) ? 'http://host.docker.internal:3000': 'http://localhost:3000'
+    ),
 ];

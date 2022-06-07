f6630f29365d10299fbdc23b66dff4904e34ee77
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-06-07 21:24:28 +0300

    Restore ovwerwriting app.js

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -60,6 +60,7 @@ class InstallCommand extends Command
             );
         }
 
+        copy(__DIR__ . '/../../stubs/resources/js/bootstrap.js', resource_path('js/bootstrap.js'));
         copy(__DIR__ . '/../../stubs/resources/views/app.blade.php', resource_path('views/app.blade.php'));
         copy(__DIR__ . '/../../stubs/package.json', base_path('package.json'));
         copy(__DIR__ . '/../Vite.php', app_path('Utils/Vite.php'));
